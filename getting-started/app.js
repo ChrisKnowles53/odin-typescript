3fa3da52fa1b1cbd5b77f42222c7b6d0f2a84047
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-12-19 08:26:18 +0000

    objects, unions, literals

--- getting-started/app.js
+++ getting-started/app.js
@@ -1,1 +1,14 @@
-console.log("im linked and updated");
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var cominedAges = combine(30, 26);
+console.log(cominedAges);
+var combinedNames = combine("Max", 2);
+console.log(combinedNames);
