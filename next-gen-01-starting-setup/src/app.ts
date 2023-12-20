854ce5703b718d4c215cf11035335575219d1b75
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-12-20 13:13:59 +0000

    using classes

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,11 +1,24 @@
-// Code goes here!
 // using rest operator to enable an unlimited numebr of argumaents to be passed into the function.  ...numbers
 // then using reduce to take the current result and add the the current value to it
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
 
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
+// const addedNumbers = add(5, 10, 2, 3.7);
+// console.log(addedNumbers);
+
+//code here!
+
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accoutning = new Department("Accounting");
+
+console.log(accoutning);
